716f82c30eaea55413202ad83811f08da3a8c297
sourav12190085 <souravrai12190085@gmail.com>
2021-03-21 21:35:02 +0600

    first commit

---
+++ TODO_4/app/src/main/java/com/example/todo_4/MainActivity2.java
@@ -0,0 +1,38 @@
+package com.example.todo_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    // private static final String LOG_TAG =MainActivity.class.getSimpleName();
+    public static final String EXTRA_REPLY = "com.gcit.todo_4.reply";
+    private TextView textview;
+    private EditText edittext;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        textview = findViewById(R.id.textView2);
+        edittext = findViewById(R.id.MessageReply);
+
+        Intent obj = getIntent();
+        String msg = obj.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        textview.setText(msg);
+    }
+
+    public void reply(View view) {
+        String message = edittext.getText().toString();
+
+        Intent Rintent = new Intent();
+        Rintent.putExtra(EXTRA_REPLY, message);
+        setResult(RESULT_OK, Rintent);
+        finish();
+    }
+}
